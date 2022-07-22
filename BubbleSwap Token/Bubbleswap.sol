9897a5d7d7da233921b30370a51678172e695828
BubbleDex <108278188+BubbleDex@users.noreply.github.com>
2022-07-22 05:20:03 +0500

    Update Bubbleswap.sol

--- BubbleSwap Token/Bubbleswap.sol
+++ BubbleSwap Token/Bubbleswap.sol
@@ -24,7 +24,6 @@ abstract contract Context {
     }
 
     function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
 }
