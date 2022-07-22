d6375165ba1cec95570b78c591cce04a9a719fd5
BubbleDex <108278188+BubbleDex@users.noreply.github.com>
2022-07-22 05:20:51 +0500

    Update Operatable.sol

--- Operatable/Operatable.sol
+++ Operatable/Operatable.sol
@@ -20,11 +20,6 @@ abstract contract Context {
         return msg.sender;
     }
 
-    function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
 
 // File: @openzeppelin/contracts/access/Ownable.sol
 
