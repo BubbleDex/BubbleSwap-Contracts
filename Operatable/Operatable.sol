14d5f179c2b5b62c5f37076a4af4a0938e6402c9
BubbleDex <108278188+BubbleDex@users.noreply.github.com>
2022-07-23 19:06:15 +0500

    Update Operatable.sol

--- Operatable/Operatable.sol
+++ Operatable/Operatable.sol
@@ -19,6 +19,7 @@ abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
         return msg.sender;
     }
+}
 
 
 // File: @openzeppelin/contracts/access/Ownable.sol
